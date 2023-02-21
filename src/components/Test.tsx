e828a6d93cfb164a2b43a1832a928bb96745c1d0
HHX <903040380@qq.com>
2023-02-21 09:54:07 +0800

    docs: 更新面试题

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
 
 export const Test = () => {
-  const a = () => {
-    console.log('测试js执行')
-
-    return '测试mdx'
+  function pMap(list: any[], mapper: Function, cur: number) {
+    cur = cur || list.length
+    let step: any = Promise.resolve()
+    do {
+      let temp = list.splice(0, cur)
+      step = step.then(() =>
+        Promise.all(
+          temp.map((i, index) =>
+            Promise.resolve(i).then((e) => mapper(e, index))
+          )
+        )
+      )
+    } while (list.length)
   }
   return (
     <div
